e73d98724c1e12dc1307868abdc7398a0badc6f0
Divye Gupta <64646670+divyegupta12@users.noreply.github.com>
2023-12-21 21:17:18 +0530

    Update vasnet_model.py

--- vasnet_model.py
+++ vasnet_model.py
@@ -33,7 +33,7 @@ class DeformableAttention(nn.Module):
         q, k, v = map(lambda t: t.view(B, N, -1), qkv)
 
         # Get offsets with convolution
-        offset = self.conv_offset(x.unsqueeze(2).unsqueeze(3)).squeeze(3).permute(0, 2, 1)
+        offset = self.conv_offset(x.unsqueeze(2).permute(0, 2, 1)).permute(0, 2, 1)
         offset = offset.view(B, N, 2, self.offset_dim)
 
         # Calculate attention scores with offsets
