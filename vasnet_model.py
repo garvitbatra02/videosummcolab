f0f2f025bbc38695fd332289e5af5412f33d569b
Divye Gupta <64646670+divyegupta12@users.noreply.github.com>
2023-12-21 22:08:51 +0530

    Update vasnet_model.py

--- vasnet_model.py
+++ vasnet_model.py
@@ -42,8 +42,10 @@ class DeformableAttention(nn.Module):
 
         # Calculate attention scores with offsets
         attn_scores = torch.matmul(q.unsqueeze(2), k.unsqueeze(1).transpose(-2, -1)) / (self.dim ** 0.5)
-        attn_scores += offset[:, :, :, :2]  # Incorporate offsets
-
+        print("Shapes: attn_scores - {}, offset - {}".format(attn_scores.shape, offset[:, :, :, :2].squeeze(-2).squeeze(-1).shape))
+        attn_scores += offset[:, :, :, :2].squeeze(-2).squeeze(-1)  # Incorporate offsets
+        
+        
         attn_weights = F.softmax(attn_scores, dim=-1)
         attn_weights = attn_weights.masked_fill(mask == 0, 0.0) if mask is not None else attn_weights
 
