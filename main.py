da5268c9e71014c10791ea16bb7a1185764985a8
Divye Gupta <64646670+divyegupta12@users.noreply.github.com>
2024-01-01 20:52:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ class AONet:
         np.random.seed(rnd_seed)
         torch.manual_seed(rnd_seed)
 
-        self.model = VASNet()
+        self.model = DeformableSelfAttention()
         self.model.eval()
         self.model.apply(weights_init)
         #print(self.model)
